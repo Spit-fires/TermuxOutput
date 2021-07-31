b13babf2d63c634d1cb88544a75fc47406cb782e
Spit-fires <84981998+Spit-fires@users.noreply.github.com>
2021-07-31 17:53:20 +0600

    Update delete.py

--- delete.py
+++ delete.py
@@ -4,7 +4,30 @@ path = "/data/data/com.termux/files/home/TermuxOutput"
 
 shutil.rmtree(path)
 
+print("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWP'dP'dWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWW"VWWWb """'.o.. .dWWWWWWWWWWWWP~~""_{WWWWWWWWWWWWWWWWWWWWWW
+WWWWWWb  """""""w.     jWWWWWWWP"  ,.--"'    .wWWWWWWWWWWWWWWWWWW
+WWWWWWWbWWWWF"""""`    WWWWWWP" .-"          {WWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWLLWP      jWWWWP"               .wwWWWWWWWWW WWWWWWWW
+WWWWW`W`W`WWWW'      jWWW'                 {WWWWWWWWWWP  `VWWWWWW
+WWWWWW, dWWWW'      jWWWW                 .wwWWP".w."WWP.WWWWWWWW
+WWWWWW' VWWWW        VWW'               _{WWWW( WWWW. P.WWWWWWWWW
+::::::':  :::         "                {  `:::::. '' ,.::::::::::
+:::::,:::  ::                                `''''  .::::::::::::
+::::::::::                                       ..,:::::::::::::
+:::::::::::   .                  ..:.      `:::::::::::::::::::::
+:::::::::::: ::   .: ::::::::   :::::::.          `::::::::::::::
+::::::::::::::' .::::::::::::.    .::::::::::::::  ::::::::::::::
+:::::::::::::'.:::::::::::::::::  ::::::::::::::'  ::::::::::::::
+:::::::'.....:.. ::::::::'......:. :::::::::'....::.`::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+
 print("Thank you for using TermuxOutput It shall now be deleted")
 
+print("please run "cd" to properly exit")
 
-cd $home
+
+
