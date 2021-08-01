5cc7368f3fcdc5d5fad883c9816a98296a2be428
Spit-fires <84981998+Spit-fires@users.noreply.github.com>
2021-08-01 22:45:16 +0600

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,26 +1,5 @@
 import os
-print('''
-WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
-WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
-WWWWWWWWWWWWWWWWWWWP'dP'dWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
-WWWWWW"VWWWb """'.o.. .dWWWWWWWWWWWWP~~""_{WWWWWWWWWWWWWWWWWWWWWW
-WWWWWWb  """""""w.     jWWWWWWWP"  ,.--"'    .wWWWWWWWWWWWWWWWWWW
-WWWWWWWbWWWWF"""""`    WWWWWWP" .-"          {WWWWWWWWWWWWWWWWWWW
-WWWWWWWWWWWWLLWP      jWWWWP"               .wwWWWWWWWWW WWWWWWWW
-WWWWW`W`W`WWWW'      jWWW'                 {WWWWWWWWWWP  `VWWWWWW
-WWWWWW, dWWWW'      jWWWW                 .wwWWP".w."WWP.WWWWWWWW
-WWWWWW' VWWWW        VWW'               _{WWWW( WWWW. P.WWWWWWWWW
-::::::':  :::         "                {  `:::::. '' ,.::::::::::
-:::::,:::  ::                                `''''  .::::::::::::
-::::::::::                                       ..,:::::::::::::
-:::::::::::   .                  ..:.      `:::::::::::::::::::::
-:::::::::::: ::   .: ::::::::   :::::::.          `::::::::::::::
-::::::::::::::' .::::::::::::.    .::::::::::::::  ::::::::::::::
-:::::::::::::'.:::::::::::::::::  ::::::::::::::'  ::::::::::::::
-:::::::'.....:.. ::::::::'......:. :::::::::'....::.`::::::::::::
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-''')
+
 
 print("
 Welcome to TermuxOutput
