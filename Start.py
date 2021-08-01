dbba5fa588ba2a4cbc9dda3d9e3138eb0ce46023
Spit-fires <84981998+Spit-fires@users.noreply.github.com>
2021-08-01 18:10:15 +0600

    Add files via upload

---
+++ Start.py
@@ -0,0 +1,30 @@
+import os
+print("
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWP'dP'dWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWW"VWWWb """'.o.. .dWWWWWWWWWWWWP~~""_{WWWWWWWWWWWWWWWWWWWWWW
+WWWWWWb  """""""w.     jWWWWWWWP"  ,.--"'    .wWWWWWWWWWWWWWWWWWW
+WWWWWWWbWWWWF"""""`    WWWWWWP" .-"          {WWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWLLWP      jWWWWP"               .wwWWWWWWWWW WWWWWWWW
+WWWWW`W`W`WWWW'      jWWW'                 {WWWWWWWWWWP  `VWWWWWW
+WWWWWW, dWWWW'      jWWWW                 .wwWWP".w."WWP.WWWWWWWW
+WWWWWW' VWWWW        VWW'               _{WWWW( WWWW. P.WWWWWWWWW
+::::::':  :::         "                {  `:::::. '' ,.::::::::::
+:::::,:::  ::                                `''''  .::::::::::::
+::::::::::                                       ..,:::::::::::::
+:::::::::::   .                  ..:.      `:::::::::::::::::::::
+:::::::::::: ::   .: ::::::::   :::::::.          `::::::::::::::
+::::::::::::::' .::::::::::::.    .::::::::::::::  ::::::::::::::
+:::::::::::::'.:::::::::::::::::  ::::::::::::::'  ::::::::::::::
+:::::::'.....:.. ::::::::'......:. :::::::::'....::.`::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+")
+
+print("
+Welcome to TermuxOutput
+
+<cat help.txt> for list of commands
+<cd> to exit TermuxOutput
+")
