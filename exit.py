40c0edeb8e7965cd29172403478a8023c34a8656
Spit-fires <84981998+Spit-fires@users.noreply.github.com>
2021-08-03 12:26:31 +0600

    Create exit.py

---
+++ exit.py
@@ -0,0 +1,11 @@
+# Import the os module
+import os
+
+# Print exit
+print("exiting...")
+
+# Change the current working directory
+os.chdir('/')
+
+# Print thank you msg
+print('thank you for using termux output')
